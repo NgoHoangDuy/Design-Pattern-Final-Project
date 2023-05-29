36cb9310ee87934e092a7f94abf4a022475a9d79
Ngô Hoàng Duy <ngohoangduy20@gmail.com>
2023-05-29 20:43:28 +0700

    init

---
+++ src/main/java/org/springframework/boot/Nhom14DesignPattern/Nhom14DesignPatternApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.boot.Nhom14DesignPattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nhom14DesignPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nhom14DesignPatternApplication.class, args);
+	}
+
+}
