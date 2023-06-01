d9393724367da76439232443376dbeed7bab24ab
Ngô Hoàng Duy <ngohoangduy20@gmail.com>
2023-06-01 17:07:43 +0700

    bridge

--- src/main/java/org/springframework/boot/Nhom14DesignPattern/StructuralPatterns/bridge/remotes/advanced/AdvancedRemote.java
+++ src/main/java/org/springframework/boot/Nhom14DesignPattern/StructuralPatterns/bridge/remotes/advanced/AdvancedRemote.java
@@ -1,4 +1,15 @@
 package org.springframework.boot.Nhom14DesignPattern.StructuralPatterns.bridge.remotes.advanced;
 
-public class AdvancedRemote {
+import org.springframework.boot.Nhom14DesignPattern.StructuralPatterns.bridge.devices.Device;
+import org.springframework.boot.Nhom14DesignPattern.StructuralPatterns.bridge.remotes.basic.BasicRemote;
+
+public class AdvancedRemote extends BasicRemote {
+    public AdvancedRemote(Device device) {
+        super.device = device;
+    }
+
+    public void mute() {
+        System.out.println("Remote: mute");
+        device.setVolume(0);
+    }
 }
